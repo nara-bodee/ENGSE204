8e3868706fb98f370771c7608eed72b0a6d72641
Narabodee <narabodeeboonlead@gmail.com>
2025-11-22 17:08:13 +0700

    Update lab1.11.java

--- lab/lab1.11.java
+++ lab/lab1.11.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 
 public class lab_11 {
 
-	public static int sumArray (int[] numbers) {
-		int s = 0;
-		for (int n : numbers) {
-			s += n; 
-		}
-		return s;
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Enter N");
-		int N = input.nextInt();
-		
-		int[] Number = new int[N];
-		
-		for (int i = 0;i<N;i++) {
-			System.out.println("Enter Number"+(i+1));
-			Number[i] = input.nextInt();
-		}
-		int r = sumArray(Number);
-		
-		System.out.println(r);
-		
-		input.close();
-	}
+    public static int sumArray(int[] numbers) {
+        int s = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            s += numbers[i]; 
+        }
+        return s;
+    }
 
+    public static void main(String[] args) {
+    	// TODO Auto-generated method stub
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int N = input.nextInt();
+
+        int[] Number = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            System.out.print("Enter Number " + i + ": ");
+            Number[i] = input.nextInt();
+        }
+
+        int r = sumArray(Number);
+        System.out.println("Sum = " + r);
+
+        input.close();
+    }
 }
