f6ac56f664d42abb9b2a34006d38bc60f214bb84
Narabodee <narabodeeboonlead@gmail.com>
2025-12-02 20:07:06 +0700

    Create lab2.2.java

---
+++ lab/lab2.2.java
@@ -0,0 +1,35 @@
+package lab2_2;
+import java.util.Scanner;
+
+class Student{
+	private String studentID;
+	private String name;
+	
+	public Student(String studentID,String name) {
+		this.studentID = studentID;
+		this.name = name;
+	}
+}	
+public class lab2_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner  input = new Scanner(System.in);
+		
+		System.out.println("Enter Student ID :");
+		String studentID = input.nextLine();
+		
+		System.out.println("Enter Name :");
+		String name = input.nextLine();
+		
+		input.close();
+		
+		Student std1 = new Student(studentID, name);
+		
+		System.out.println(studentID);
+		System.out.println(name);
+		
+		
+	}
+
+}
